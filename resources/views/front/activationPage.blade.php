b61a101c3c194e52a70f4efd1e4b0447e08d7314
shaimaaAhmed <ahmedshaimaa39@gmail.com>
2020-07-06 14:28:02 +0200

    update

--- resources/views/front/activationPage.blade.php
+++ resources/views/front/activationPage.blade.php
@@ -22,6 +22,20 @@
             </ul>
         </div>
     @endif
+        @if(Session::has('error'))
+            <div class="row mr-2 ml-2" >
+                <button type="text" class="btn btn-lg btn-block btn-outline-danger mb-2"
+                        id="type-error">{{Session::get('error')}}
+                </button>
+            </div>
+        @endif
+        @if(Session::has('success'))
+            <div class="row mr-2 ml-2">
+                <button type="text" class="btn btn-lg btn-block btn-outline-success mb-2"
+                        id="type-error">{{Session::get('success')}}
+                </button>
+            </div>
+        @endif
     @include('flash::message')
     <div class="card pt-3">
         <div class="container">
